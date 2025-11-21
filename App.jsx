212ccfb413975a6c3d6ffe228e932d84ccf99cdf
erlano13 <erlanosousa10@gmail.com>
2025-11-21 11:43:56 -0300

    Adicionando navbar com menu hambúrguer

--- App.jsx
+++ App.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { AdditionRequests } from './src/pages/Admin/AdditionRequests';
 import { Footer } from './src/components/Footer';
+import { Navbar } from './src/components/Navbar';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       
+      <Navbar />
       <AdditionRequests />
       <Footer />
       
